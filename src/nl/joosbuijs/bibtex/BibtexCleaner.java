c6cbac655f800721a6019a1f8475c4d6466b5fbf
Tim Felgentreff <timfelgentreff@gmail.com>
2015-12-03 15:56:11 +0100

    refactor for filtering and merging

--- src/nl/joosbuijs/bibtex/BibtexCleaner.java
+++ src/nl/joosbuijs/bibtex/BibtexCleaner.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Collection;
 
 import org.joda.time.DateTime;
 
@@ -46,7 +48,7 @@ public class BibtexCleaner {
     static class EntryComp implements Comparator<BibtexAbstractEntry> {
 	public int compare(BibtexAbstractEntry o1, BibtexAbstractEntry o2) {
 	    if (o1 instanceof BibtexEntry && o2 instanceof BibtexEntry) {
-		return ((BibtexEntry)o1).getEntryKey().compareTo(((BibtexEntry)o2).getEntryKey());
+		return ((BibtexEntry)o1).getEntryKey().toLowerCase().compareTo(((BibtexEntry)o2).getEntryKey().toLowerCase());
 	    } else if (o1 instanceof BibtexEntry) {
 		return -1;
 	    } else if (o2 instanceof BibtexEntry) {
@@ -68,207 +70,149 @@ public class BibtexCleaner {
 	}
     }
 
-    public static int maxNrEntries2Parse = -1;//10;
-    public static Set<String> keys = new HashSet<String>();
-    public static int AUTHORS_IN_QUERY_MAX_ELEMENTS = 10; //manifesto causes a HTTP 414 error: URL too long :D
-
-    public static void prepareKeys(BibtexFile file) {
-	keys.clear();
-	for (BibtexAbstractEntry potentialEntry : file.getEntries()) {
-	    if (potentialEntry instanceof BibtexEntry) {
-		keys.add(((BibtexEntry) potentialEntry).getEntryKey().toLowerCase());
+    public static void mergeEntries(BibtexEntry entry, BibtexEntry newEntry) {
+	for (String fieldKey : newEntry.getFields().keySet()) {
+	    if (!entry.getFields().containsKey(fieldKey)) {
+		entry.addFieldValue(fieldKey, newEntry.getFieldValue(fieldKey));
 	    }
+	}
+	if (!entry.getEntryKey().toLowerCase().equals(newEntry.getEntryKey().toLowerCase())) {
+	    entry.addFieldValue("AlternateName" + entry.getFields().size(), newbibtex.makeString(newEntry.getEntryKey()));
 	}
     }
 
-    public static void SaveFiles(String sourceBibFile, BibtexFile newbibtex, BibtexFile newCrossrefbibtex) throws FileNotFoundException {
-	String newBibFile = sourceBibFile.replace(".bib", "_cleaned.bib");
-	System.out.println("Saving clean Bibtex to " + newBibFile);
-	newbibtex.printBibtex(new PrintWriter(new File(newBibFile)));
 
-	String newCrossrefBibFile = newBibFile.replace(".bib", "_crossref.bib");
-	System.out.println("Saving clean crossref Bibtex to " + newCrossrefBibFile);
-	newCrossrefbibtex.printBibtex(new PrintWriter(new File(newCrossrefBibFile)));
+    public static BibtexEntry mergeDBLPEntry(BibtexEntry entry, BibtexEntry newEntry) {
+	mergeEntries(newEntry, entry);
+	newEntry.addFieldValue(DBLPKey, newbibtex.makeString(newEntry.getEntryKey()));
+	newEntry.setEntryKey(entry.getEntryKey());
+	return newEntry;
     }
 
-    //TODO: sanity check for obtained search result w.r.t. current bibtex entry
-    //TODO: post process: authors!
-    //TODO: add option to escape special characters (% #) in url and doi fields
+    public static void addEntry(BibtexEntry entry) {
+	if (knownEntries.get(entry.getEntryKey().toLowerCase()) != null) {
+	    mergeEntries(knownEntries.get(entry.getEntryKey().toLowerCase()), entry);
+	    entry = knownEntries.get(entry.getEntryKey().toLowerCase());
+	}
+	if (entry.getFields().containsKey(DBLPKey)
+	    && knownEntries.get(entry.getFieldValue(DBLPKey).toString().toLowerCase()) != null) {
+	    knownEntries.put(entry.getEntryKey().toLowerCase(), entry);
+	    mergeEntries(knownEntries.get(entry.getFieldValue(DBLPKey).toString().toLowerCase()), entry);
+	} else {
+	    knownEntries.put(entry.getEntryKey().toLowerCase(), entry);
+	}
+    }
+
+    public static BibtexEntry[] cleanEntry(BibtexEntry entry, DBLPQueryParser dblpQuery) {
+	System.out.println("Cleaning entry " + entry.getEntryKey());
+	try {
+	    Pair<BibtexEntry, BibtexEntry> firstMatch = dblpQuery.getFirstEntryForQuery(entry.getFieldValue("title").toString());
+	    System.out.println(" Found " + dblpQuery.getNrOfResults() + " results on DBLP");
+	    if (dblpQuery.getNrOfResults() > 1) {
+		System.out.println("Fine-tuning...");
+		firstMatch = dblpQuery.getFirstEntryForQuery(entry.getFieldValue("title").toString() + " " + BibtexCleaner.getAuthorNames(entry));
+		if (dblpQuery.getNrOfResults() == 0) {
+		    firstMatch = dblpQuery.getFirstEntryForQuery(entry.getFieldValue("title").toString());
+		}
+	    }
+
+	    if (dblpQuery.getNrOfResults() > 10 || dblpQuery.getNrOfResults() < 1) {
+		System.out.println(dblpQuery.getNrOfResults() + " results, using old entry...");
+		return new BibtexEntry[] { entry };
+	    } else if (dblpQuery.getNrOfResults() > 1) {
+		System.out.println("We got too many results, remembering and letting user choose best option later.");
+		BibtexEntry[] r = new BibtexEntry[dblpQuery.getNrOfResults() + 1];
+		r[0] = entry;
+		for (int i = 0; i < dblpQuery.getNrOfResults(); i++) {
+		    r[i + 1] = dblpQuery.getEntryByIndex(i).getFirst();
+		}
+		return r;
+	    } else {
+		return new BibtexEntry[] { mergeDBLPEntry(entry, firstMatch.getFirst()) };
+	    }
+	} catch(Exception e) {
+	    // just keep the entry and keep going
+	    System.out.println("Exception, uhhh");
+	    return new BibtexEntry[] { entry };
+	}
+    }
+
+    public static final String DBLPKey = "DBLPkey";
+    public static HashMap<String, BibtexEntry> knownEntries = new HashMap<String, BibtexEntry>();
+    public static int AUTHORS_IN_QUERY_MAX_ELEMENTS = 7; //manifesto causes a HTTP 414 error: URL too long :D
+
+    public static void SaveFiles(String sourceBibFile) throws FileNotFoundException {
+	String newBibFile = sourceBibFile.replace(".bib", "_cleaned.bib");
+	newbibtex.printBibtex(new PrintWriter(new File(newBibFile)));
+    }
+
+    public static List<BibtexEntry> prepareEntryList(String sourceBibFile, String f) throws ParseException, FileNotFoundException, IOException {
+	// Load file
+	BibtexFile file = new BibtexFile();
+	(new BibtexParser(false)).parse(file, new FileReader(sourceBibFile));
+	System.out.println("loaded dirty Bibtex file " + sourceBibFile);
+
+	// load good entry list
+	BibtexFile rfile = new BibtexFile();
+	Set<String> goodEntries = new HashSet<String>();
+	(new BibtexParser(false)).parse(rfile, new FileReader(f));
+	goodEntries.clear();
+	for (BibtexAbstractEntry potentialEntry : rfile.getEntries()) {
+	    if (potentialEntry instanceof BibtexEntry) {
+		goodEntries.add(((BibtexEntry) potentialEntry).getEntryKey().toLowerCase());
+	    }
+	}
+	System.out.println("loaded filter Bibtex file " + f);
+
+	// merge duplicates and filter out
+	HashMap<String, BibtexEntry> subEntries = new HashMap<String, BibtexEntry>();
+	for (BibtexAbstractEntry potentialEntry : file.getEntries()) {
+	    if (potentialEntry instanceof BibtexEntry) {
+		String k = ((BibtexEntry)potentialEntry).getEntryKey().toLowerCase();
+		if (goodEntries.contains(k)) {
+		    if (subEntries.get(k) != null) {
+			mergeEntries(subEntries.get(k), ((BibtexEntry)potentialEntry));
+		    } else {
+			subEntries.put(k, (BibtexEntry)potentialEntry);
+		    }
+		}
+	    }
+	}
+
+	// Sort by key
+	return sortedEntries(subEntries.values());
+    }
+
+    public static List<BibtexEntry> sortedEntries(Collection<BibtexEntry> c) {
+	ArrayList<BibtexEntry> entries = new ArrayList<BibtexEntry>();
+	entries.addAll(c);
+	Collections.sort(entries, new EntryComp());
+	return entries;
+    }
+
+    static BibtexFile newbibtex = new BibtexFile();
 
     public static void main(String... strings) {
 	if (strings.length < 1 || strings.length > 2) {
 	    System.err.println("\nMust pass input bibtex file as first argument, and optionally a restricted file as second");
 	    return;
 	}
-	Set<String> knownKeys = new HashSet<String>();
 
 	try {
-	    /*
-	     * Load the dirty bibliography
-	     */
-	    String sourceBibFile = strings[0];
-	    FileReader in = new FileReader(sourceBibFile);
-	    BibtexParser parser = new BibtexParser(false);
-	    BibtexFile file = new BibtexFile();
-	    parser.parse(file, in);
-
-	    System.out.println("loaded dirty Bibtex file " + sourceBibFile);
-
-	    if (strings.length == 2) {
-		BibtexFile rfile = new BibtexFile();
-		(new BibtexParser(false)).parse(rfile, new FileReader(strings[1]));
-		prepareKeys(rfile);
-	    }
-
-	    /*
-	     * Create a the new bibtex file
-	     */
-	    BibtexFile newbibtex = new BibtexFile();
-	    BibtexFile newCrossrefbibtex = new BibtexFile();
-
-	    /*
-	     * Now for each original entry
-	     */
-	    DBLPQueryParser dblpQuery = new DBLPQueryParser();
-	    Vector<BibtexEntry[]> rememberedEntries = new Vector<BibtexEntry[]>();
-
-	    if (strings.length == 1) {
-		System.out.println("There are " + file.getEntries().size() + " entries to parse");
-	    } else if (strings.length == 2) {
-		System.out.println("There are " + keys.size() + " entries to parse");
-	    }
-
-	    System.out.println("We expect to finish around "
-			       + DateTime.now().plusSeconds(2 * Math.min(maxNrEntries2Parse > 0 ? maxNrEntries2Parse : Integer.MAX_VALUE, file
-									 .getEntries().size()) * DBLPQueryParser.timeout));
-
+	    String sourceBibFile = strings[0];	    
 	    String toplevelComment = String.format("%% This bibTex file was cleaned by BibtexCleaner by Joos Buijs %n"
 						   + "%% Cleaned version of %s%n" + "%% Cleaned on %s%n" + "%% %n%n", sourceBibFile, DateTime.now());
-
 	    newbibtex.addEntry(newbibtex.makeToplevelComment(toplevelComment));
-	    newCrossrefbibtex.addEntry(newCrossrefbibtex.makeToplevelComment(toplevelComment));
-	    newCrossrefbibtex.addEntry(newCrossrefbibtex.makeToplevelComment("%%Crossreference bibtex file!"));
+	    List<BibtexEntry> entries = prepareEntryList(strings[0], strings.length == 2 ? strings[1] : strings[0]);
 
-	    int nrEntriesCleaned = 0;
-	    ArrayList<BibtexAbstractEntry> entries = new ArrayList<BibtexAbstractEntry>();
-	    entries.addAll(file.getEntries());
-	    Collections.sort(entries, new EntryComp());
-	    for (BibtexAbstractEntry potentialEntry : entries) {
-		SaveFiles(sourceBibFile, newbibtex, newCrossrefbibtex); // always save files again
-		if (potentialEntry instanceof BibtexEntry) {
-		    BibtexEntry entry = (BibtexEntry) potentialEntry;
-		    if (strings.length == 2 && !keys.contains(entry.getEntryKey().toLowerCase())) {
-			System.out.println("Skipping entry " + entry.getEntryKey());
-			continue;
-		    }
-		    try {
-			System.out.println("Cleaning entry " + entry.getEntryKey());
-
-			//Find matching entry on DBLP
-			Pair<BibtexEntry, BibtexEntry> firstMatch = dblpQuery.getFirstEntryForQuery(entry.getFieldValue(
-															"title").toString());
-			System.out.println(" Found " + dblpQuery.getNrOfResults() + " results on DBLP");
-
-			//add authors (last name) etc.
-			if (dblpQuery.getNrOfResults() > 1) {
-			    System.out.println(" Fine-tuning query...");
-			    firstMatch = dblpQuery.getFirstEntryForQuery(entry.getFieldValue("title").toString() + " "
-									 + BibtexCleaner.getAuthorNames(entry));
-
-			    if (dblpQuery.getNrOfResults() == 0) {
-				System.out.println(" Executing title query only and let user pick");
-				firstMatch = dblpQuery.getFirstEntryForQuery(entry.getFieldValue("title").toString());
-			    }
-			}
-
-			//Now given the 'best' query we got:
-			if (dblpQuery.getNrOfResults() > 10) {
-			    System.out.println(" There were too many results. Using old entry...");
-			    BibtexFile bibtexFile;
-			    if (BibtexCleaner.isCrossrefType(entry)) {
-				bibtexFile = newCrossrefbibtex;
-			    } else {
-				bibtexFile = newbibtex;
-			    }
-			    if (!knownKeys.contains(entry.getEntryKey())) {
-				bibtexFile.addEntry(newbibtex.makeToplevelComment("NOT CLEANED ENTRY (too many results):"));
-				bibtexFile.addEntry(entry);
-				knownKeys.add(entry.getEntryKey().toLowerCase());
-			    }
-			    continue;
-			}
-			if (dblpQuery.getNrOfResults() > 1) {
-			    System.out.println(" We got too many results, remembering and letting user choose best option later.");
-			    BibtexEntry[] r = new BibtexEntry[dblpQuery.getNrOfResults() + 1];
-			    r[0] = entry;
-			    for (int i = 0; i < dblpQuery.getNrOfResults(); i++) {
-				r[i + 1] = dblpQuery.getEntryByIndex(i).getFirst();
-			    }
-			    rememberedEntries.add(r);
-			} else if (dblpQuery.getNrOfResults() == 0) {
-			    //No results found, can not improve
-			    System.out.println(" No results found, copying old entry to new bibtex file");
-
-			    BibtexFile bibtexFile;
-			    if (BibtexCleaner.isCrossrefType(entry)) {
-				bibtexFile = newCrossrefbibtex;
-			    } else {
-				bibtexFile = newbibtex;
-			    }
-			    if (!knownKeys.contains(entry.getEntryKey())) {
-				bibtexFile.addEntry(newbibtex.makeToplevelComment("NOT CLEANED ENTRY:"));
-				bibtexFile.addEntry(entry);
-				knownKeys.add(entry.getEntryKey().toLowerCase());
-			    }
-			    continue;
-			} else {
-			    nrEntriesCleaned++;
-
-			    //Update key of DBLP entry to original key
-			    BibtexEntry newEntry = firstMatch.getFirst();
-			    if (knownKeys.contains(entry.getEntryKey().toLowerCase())
-				|| knownKeys.contains(newEntry.getEntryKey().toLowerCase())) {
-				System.out.println("Duplicate, skipping...");
-				continue;
-			    }
-			    knownKeys.add(newEntry.getEntryKey().toLowerCase());
-			    knownKeys.add(entry.getEntryKey().toLowerCase());
-			    newEntry.addFieldValue("DBLPkey", newbibtex.makeString(newEntry.getEntryKey()));
-			    newEntry.setEntryKey(entry.getEntryKey());						   
-
-			    //Add fields of original entry to the new entry if not contained
-			    for (String fieldKey : entry.getFields().keySet()) {
-				if (!newEntry.getFields().containsKey(fieldKey)) {
-				    newEntry.addFieldValue(fieldKey, entry.getFieldValue(fieldKey));
-				}
-			    }
-
-			    //Add new entry to new bibtex 
-			    if (!knownKeys.contains(newEntry.getEntryKey())) {
-				System.out.println(" Adding new entry ");
-				if (BibtexCleaner.isCrossrefType(newEntry)) {
-				    newCrossrefbibtex.addEntry(newEntry);
-				} else {
-
-				    newbibtex.addEntry(newEntry);
-				}
-				if (firstMatch.getSecond() != null) {
-				    System.out.println(" Adding new venue/crossref entry");
-				    newCrossrefbibtex.addEntry(firstMatch.getSecond());
-				}
-			    }
-			}
-		    } catch(Exception e) {
-			// just keep the entry and keep going
-			newbibtex.addEntry(potentialEntry);
-		    }
+	    DBLPQueryParser dblpQuery = new DBLPQueryParser();
+	    Vector<BibtexEntry[]> rememberedEntries = new Vector<BibtexEntry[]>();
+	    for (BibtexEntry potentialEntry : entries) {
+		BibtexEntry[] results = cleanEntry(potentialEntry, dblpQuery);
+		if (results.length == 1) {
+		    addEntry(results[0]);
 		} else {
-		    //We keep 'failed' potential entries... (like JabRef comments and such)
-		    newbibtex.addEntry(potentialEntry);
-		}
-		
-		if (maxNrEntries2Parse >= 0 && nrEntriesCleaned >= maxNrEntries2Parse) {
-		    break;
+		    rememberedEntries.add(results);
 		}
 	    }
 
@@ -285,85 +229,19 @@ public class BibtexCleaner {
 		Scanner sysoIn = new Scanner(System.in);
 		String answer = sysoIn.nextLine();
 		if (answer.isEmpty()) {
-		    System.out.println(" Using old entry");
-		    BibtexFile bibtexFile;
-		    if (BibtexCleaner.isCrossrefType(rlist[0])) {
-			bibtexFile = newCrossrefbibtex;
-		    } else {
-			bibtexFile = newbibtex;
-		    }
-		    if (!knownKeys.contains(rlist[0].getEntryKey())) {
-			bibtexFile.addEntry(newbibtex.makeToplevelComment("NOT CLEANED ENTRY (by user choice):"));
-			bibtexFile.addEntry(rlist[0]);
-			knownKeys.add(rlist[0].getEntryKey().toLowerCase());
-		    }
-		    continue;
+		    addEntry(rlist[0]);
 		} else {
 		    int correctIndex = Integer.parseInt(answer);
 		    System.out.println(" Using entry at index " + correctIndex);
-
-		    nrEntriesCleaned++;
-
-		    //Update key of DBLP entry to original key
-		    BibtexEntry newEntry = rlist[correctIndex];
-		    if (knownKeys.contains(rlist[0].getEntryKey().toLowerCase())
-			|| knownKeys.contains(newEntry.getEntryKey().toLowerCase())) {
-			System.out.println("Duplicate, skipping...");
-			continue;
-		    }
-		    knownKeys.add(newEntry.getEntryKey().toLowerCase());
-		    knownKeys.add(rlist[0].getEntryKey().toLowerCase());
-		    newEntry.addFieldValue("DBLPkey", newbibtex.makeString(newEntry.getEntryKey()));
-		    newEntry.setEntryKey(rlist[0].getEntryKey());
-
-		    //Add fields of original entry to the new entry if not contained
-		    for (String fieldKey : rlist[0].getFields().keySet()) {
-			if (!newEntry.getFields().containsKey(fieldKey)) {
-			    newEntry.addFieldValue(fieldKey, rlist[0].getFieldValue(fieldKey));
-			}
-		    }
-
-		    //Add new entry to new bibtex
-		    if (!knownKeys.contains(newEntry.getEntryKey())) {
-			System.out.println(" Adding new entry ");
-			if (BibtexCleaner.isCrossrefType(newEntry)) {
-			    newCrossrefbibtex.addEntry(newEntry);
-			} else {
-			    newbibtex.addEntry(newEntry);
-			}
-		    }
+		    addEntry(mergeDBLPEntry(rlist[0], rlist[correctIndex]));
 		}
 	    }
 
-
-	    /*
-	     * Save the new bibtex file
-	     */
-	    SaveFiles(sourceBibFile, newbibtex, newCrossrefbibtex);
-
+	    for (BibtexEntry e : sortedEntries(knownEntries.values())) {
+		newbibtex.addEntry(e);
+	    }
+	    SaveFiles(sourceBibFile);
 	    System.out.println("DONE");
-	    /*-
-	      for (Iterator it = file.getEntries().iterator(); it.hasNext();) {
-	      Object potentialEntry = it.next();
-	      if (!(potentialEntry instanceof BibtexEntry))
-	      continue;
-	      BibtexEntry entry = (BibtexEntry) potentialEntry;
-	      BibtexString authorString = (BibtexString) entry.getFieldValue("author");
-	      if (authorString == null)
-	      continue;
-	      String content = authorString.getContent();
-	      String tokens[] = content.split("\\s++");
-	      for (int i = 0; i < tokens.length; i++) {
-	      if (tokens[i].toLowerCase().equals("and")) {
-	      System.out.println();
-	      continue;
-	      } else if (tokens[i].toLowerCase().equals("others"))
-	      continue;
-	      System.out.print(tokens[i] + " ");
-	      }
-	      System.out.println();
-	      }/**/
-
 	} catch (ParseException e) {
 	    e.printStackTrace();
 	} catch (FileNotFoundException e) {
